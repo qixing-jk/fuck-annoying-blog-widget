81dd745ef38f696ddb061c07e87a2d54adbb5740
anime <street-anime-olive@duck.com>
2025-07-28 21:13:27 +0800

    feat(propertyInterceptor): refactor service to support multiple targets
    - rename target to targetList in PropertyInterceptorPayload interface
    - implement groupRegistrations function to handle both single and array targets
    - extract setter and getter logic into separate factory functions

--- src/features/disableTitleChange.ts
+++ src/features/disableTitleChange.ts
@@ -3,7 +3,7 @@ import { PropertyInterceptorPayload } from '../services/propertyInterceptorServi
 
 export const propertyInterceptors: PropertyInterceptorPayload[] = [
   {
-    target: document,
+    targetList: document,
     propertyName: 'onvisibilitychange',
     setter: () => true, // 直接返回 true 来阻止任何赋值
     getter: () => undefined, // 返回 undefined 表示我们不关心谁来读取它，但这个 getter 必须存在才能覆盖
--- src/features/removeCustomContextMenu.ts
+++ src/features/removeCustomContextMenu.ts
@@ -2,13 +2,13 @@ import { PropertyInterceptorPayload } from '../services/propertyInterceptorServi
 
 export const propertyInterceptors: PropertyInterceptorPayload[] = [
   {
-    target: document,
+    targetList: document,
     propertyName: 'oncontextmenu',
     setter: () => true, // 直接返回 true 来阻止任何赋值
     getter: () => undefined, // 返回 undefined 表示我们不关心谁来读取它，但这个 getter 必须存在才能覆盖
   },
   {
-    target: window,
+    targetList: window,
     propertyName: 'oncontextmenu',
     setter: () => true, // 直接返回 true 来阻止任何赋值
     getter: () => undefined, // 返回 undefined 表示我们不关心谁来读取它，但这个 getter 必须存在才能覆盖
