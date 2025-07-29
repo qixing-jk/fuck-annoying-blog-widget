b7aefe193c4329cdd26e7be09ebba1214310b1d3
anime <street-anime-olive@duck.com>
2025-07-29 23:05:22 +0800

    feat: enhance meting music player removal logic

--- src/features/removeMusicPlayer.ts
+++ src/features/removeMusicPlayer.ts
@@ -1,21 +1,24 @@
-import { watchAndDestroy } from '../utils'
+import { onDOMReady, watchAndDestroy } from '../utils'
 
-function pauseMetingMusicPlayer() {
-  const metingPlayerPauseButton = document.querySelector('.aplayer-pause') as HTMLButtonElement
+function pauseMetingMusicPlayer(element?: Element) {
+  const metingPlayerPauseButton = (element ||
+    document.querySelector('.aplayer-pause')) as HTMLButtonElement
+  console.log('[Meting Player Blocker] metingPlayerPauseButton', metingPlayerPauseButton)
   if (metingPlayerPauseButton) {
-    metingPlayerPauseButton.click()
+    console.log('[Meting Player Blocker] meting player paused.')
+    metingPlayerPauseButton?.click()
   }
 }
 
 function removeMetingMusicPlayer() {
-  const ELEMENT_TAG = 'meting-js'
-  pauseMetingMusicPlayer()
-  watchAndDestroy(`${ELEMENT_TAG}`, () => {
-    const metingPlayers = document.getElementsByTagName(ELEMENT_TAG)
-    for (const metingPlayer of metingPlayers) {
-      metingPlayer.remove()
-      console.log('[Meting Player Blocker] meting player removed.')
-    }
+  const ELEMENT_TAG = 'meting-js .aplayer-pause'
+  onDOMReady(() => {
+    watchAndDestroy(`${ELEMENT_TAG}`, {
+      beforeFound: (element) => {
+        pauseMetingMusicPlayer(element)
+      },
+      keep: true,
+    })
   })
 }
 
