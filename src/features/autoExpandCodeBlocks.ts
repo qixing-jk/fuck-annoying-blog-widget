a40aab8d3613f85fbed1647c1ecc40942a4e7128
anime <street-anime-olive@duck.com>
2025-08-02 20:13:25 +0800

    feat(features): add auto expand code blocks functionality

---
+++ src/features/autoExpandCodeBlocks.ts
@@ -0,0 +1,31 @@
+/**
+ * 通用自动展开代码块功能
+ * 适配常见博客/论坛/文档页面的折叠代码块，自动展开，低性能消耗
+ */
+import { onDOMReady } from '../utils'
+
+const BUTTON_SELECTORS = ['.show-btn']
+
+function internalAutoExpandCodeBlocks() {
+  // 自动点击“展开”按钮（如有）
+  for (const sel of BUTTON_SELECTORS) {
+    const elementNodeList = document.querySelectorAll(sel)
+    if (elementNodeList) {
+      console.log('自动展开代码块')
+      elementNodeList.forEach((btn) => {
+        if (btn instanceof HTMLElement) {
+          btn.click()
+        }
+      })
+      break
+    }
+  }
+}
+
+function autoExpandCodeBlocks() {
+  onDOMReady(() => {
+    internalAutoExpandCodeBlocks()
+  }, 3000)
+}
+
+export default autoExpandCodeBlocks
