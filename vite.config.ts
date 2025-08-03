07878dd7eae03fbe86e84ddd7eb6570de1367d2b
anime <street-anime-olive@duck.com>
2025-08-03 16:50:43 +0800

    feat: add build optimization version support

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,39 @@ import AutoImport from 'unplugin-auto-import/vite'
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    AutoImport({
-      imports: [util.unimportPreset],
-    }),
-    monkey({
-      entry: 'src/main.tsx',
-      userscript: {
-        name: {
-          '': 'Personal Blog Annoying Features and Pendant Purification',
-          'zh-CN': '个人博客恼人功能和挂件净化',
+export default defineConfig(({ mode }) => {
+  const IsNeedOptimize = mode === 'optimize'
+  return {
+    plugins: [
+      react(),
+      AutoImport({
+        imports: [util.unimportPreset],
+      }),
+      monkey({
+        entry: 'src/main.tsx',
+        userscript: {
+          name: {
+            '': 'Personal Blog Annoying Features and Pendant Purification',
+            'zh-CN': '个人博客恼人功能和挂件净化',
+          },
+          version: pkg.version,
+          description: {
+            '': 'purify personal blogs by removing or disabling common annoying widgets and effects.',
+            'zh-CN': '净化个人博客，自动移除或禁用常见的烦人挂件和特效。',
+          },
+          icon: 'https://raw.githubusercontent.com/qixing-jk/fuck-annoying-blog-widget/refs/heads/main/src/assets/logo.jpg',
+          namespace: 'https://github.com/qixing-jk/fuck-annoying-blog-widget',
+          match: ['*://*/*'],
+          'run-at': 'document-start',
         },
-        version: pkg.version,
-        description: {
-          '': 'purify personal blogs by removing or disabling common annoying widgets and effects.',
-          'zh-CN': '净化个人博客，自动移除或禁用常见的烦人挂件和特效。',
+        build: {
+          fileName: (pkg.name ?? 'monkey') + (IsNeedOptimize ? '.optimized' : '') + '.user.js',
         },
-        icon: 'https://raw.githubusercontent.com/qixing-jk/fuck-annoying-blog-widget/refs/heads/main/src/assets/logo.jpg',
-        namespace: 'https://github.com/qixing-jk/fuck-annoying-blog-widget',
-        match: ['*://*/*'],
-        'run-at': 'document-start',
-      },
-    }),
-  ],
+      }),
+    ],
+    build: {
+      minify: IsNeedOptimize,
+      emptyOutDir: false,
+    },
+  }
 })
